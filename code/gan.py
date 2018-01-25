8634209a5656013907914e004f09ecae07cdd9df
aryopg <aryopg@gmail.com>
2018-01-25 17:57:08 +0700

    fix bug

--- code/gan.py
+++ code/gan.py
@@ -20,7 +20,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-from helpers.datagenerator import DataGenerator, FakeDataGenerator
+from helpers.datagenerator_keras import DataGenerator, FakeDataGenerator
 
 # from generator import GeneratorConvEncDec, GeneratorEncDec, GeneratorEncDecTeacherForcing, GeneratorVan
 # from generator import GeneratorEncDecTeacherForcingNoAtt as GeneratorEncDecTeacherForcing
@@ -41,6 +41,10 @@ processed_data_dir = '../data_keras'
 train_data_dir = os.path.join(processed_data_dir, 'train')
 test_data_dir = os.path.join(processed_data_dir, 'test')
 INV_LEXICON_DICTIONARY = pickle.load(open('../data_keras/lexicon-dict-inverse.pkl', 'rb'))
+
+pretrain_netD = 'last_checkpoint_vanilla_pretrain_encdec_Disc.pth.tar'
+pretrain_netG = 'last_checkpoint_vanilla_pretrain_encdec_Gen.pth.tar'
+train_GAN = 'last_checkpoint_vanilla_encdec_25Jan.pth.tar'
 
 def decode(out):
     ret = []
@@ -63,10 +67,206 @@ def decode_motion(out):
         ret.append(" ".join(sent))
     return ret
 
-def save_checkpoint(state, filename='last_checkpoint_vanilla_encdec_25Jan.pth.tar'):
+def save_checkpoint(state, filename=train_GAN):
     if os.path.isfile(filename):
         os.remove(filename)
     torch.save(state, filename)
+
+def save_pretrain_checkpoint_D(state, filename=pretrain_netD):
+    if os.path.isfile(filename):
+        os.remove(filename)
+    torch.save(state, filename)
+
+def save_pretrain_checkpoint_G(state, filename=pretrain_netG):
+    if os.path.isfile(filename):
+        os.remove(filename)
+    torch.save(state, filename)
+
+def pretrain(run_name, netD, netG, motion_length, claim_length, embedding_dim, hidden_dim_G, hidden_dim_D, lam, batch_size, epochs, num_words, train_data_dir, test_data_dir):
+    # netG = GeneratorEncDec(batch_size, num_words, claim_length, hidden_dim_G, embedding_dim)
+    nllloss = batchNLLLoss()
+    bceloss = nn.BCELoss()
+
+    if use_cuda:
+        nllloss = nllloss.cuda(gpu)
+        bceloss = bceloss.cuda(gpu)
+        netG = netG.cuda(gpu)
+        netD = netD.cuda(gpu)
+
+    optimizerG = optim.Adam(netG.parameters(), lr=1e-4)#, betas=(0.5, 0.9))
+    optimizerD = optim.Adam(netD.parameters(), lr=1e-4)#, betas=(0.5, 0.9))
+
+    # one = torch.FloatTensor([1])
+    # mone = one * -1
+    # if use_cuda:
+    #     one = one.cuda(gpu)
+    #     mone = mone.cuda(gpu)
+
+    num_train_data = len(os.listdir(train_data_dir))
+    num_test_data = len(os.listdir(test_data_dir))
+
+    start_epoch = 0
+    if os.path.isfile(pretrain_netD):
+        print("=> loading checkpoint '{}'".format(pretrain_netD))
+        checkpoint = torch.load(pretrain_netD)
+        start_epoch = checkpoint['epoch']
+        netD.load_state_dict(checkpoint['D_state_dict'])
+        optimizerD.load_state_dict(checkpoint['optimizerD'])
+        print("=> loaded checkpoint '{}' (epoch {})"
+              .format(pretrain_netD, checkpoint['epoch']))
+
+    ###########################
+    # (1) Pretrain D network  #
+    ###########################
+
+    pretraining_generator = FakeDataGenerator(lexicon_count=lexicon_count, motion_length=motion_length, claim_length=claim_length, num_data=num_train_data, data_dir=train_data_dir, batch_size=batch_size, shuffle=True)
+    pretraining_test_generator = FakeDataGenerator(lexicon_count=lexicon_count, motion_length=motion_length, claim_length=claim_length, num_data=num_test_data, data_dir=test_data_dir, batch_size=batch_size, shuffle=True)
+    netD.batch_size = netD.batch_size*2
+
+    for iteration in xrange(start_epoch,epochs+1):
+        print(" >>> Epoch : %d/%d" % (iteration+1,epochs))
+        start_time = time.time()
+
+        training_steps = num_train_data//batch_size
+        for training_step in range(training_steps):
+            llprint("\rPretraining Discriminator : Training step Discriminator %d/%d" % (training_step+1, training_steps))
+            netD.zero_grad()
+
+            _claim, _label = pretraining_generator.generate().next()
+            _claim = np.asarray(_claim)
+            _label = np.asarray(_label)
+            claims = torch.from_numpy(_claim)
+            labels = torch.from_numpy(_label)
+
+            if use_cuda:
+                claims = claims.cuda(gpu)
+                labels = labels.cuda(gpu)
+            claims_v = autograd.Variable(claims)
+            labels_v = autograd.Variable(labels).unsqueeze(1)
+
+            _, D_out = netD(claims_v)
+
+            D_loss = bceloss(D_out, labels_v)
+
+            D_loss.backward()
+            D_loss = D_loss
+            optimizerD.step()
+
+        testing_steps = num_test_data//batch_size
+        D_loss_test = 0
+        for testing_step in range(testing_steps):
+            llprint("\Testing step Discriminator %d/%d" % (testing_step+1, testing_steps))
+            netD.zero_grad()
+
+            _claim, _label = pretraining_test_generator.generate().next()
+            _claim = np.asarray(_claim)
+            _label = np.asarray(_label)
+            claims = torch.from_numpy(_claim)
+            labels = torch.from_numpy(_label)
+
+            if use_cuda:
+                claims = claims.cuda(gpu)
+                labels = labels.cuda(gpu)
+            claims_v = autograd.Variable(claims)
+            labels_v = autograd.Variable(labels).unsqueeze(1)
+
+            _, D_out = netD(claims_v)
+
+            D_loss_test += bceloss(D_out, labels_v).cpu().data.numpy()[0]
+
+        print('Iter: {}; D_loss: {:.4}'.format(iteration+1, D_loss_test/testing_steps))
+        with open('pretrain_D_loss_logger.txt', 'a') as out:
+            out.write(str(iteration+1) + ',' + str(D_loss.cpu().data.numpy()[0]) + '\n')
+
+        if iteration % 10 == 0:
+            save_pretrain_checkpoint_D({
+                'epoch': iteration + 1,
+                'D_state_dict': netD.state_dict(),
+                'optimizerD' : optimizerD.state_dict(),
+            })
+
+    ###########################
+    # (2) Pretrain G network  #
+    ###########################
+
+    start_epoch = 0
+    if os.path.isfile(pretrain_netG):
+        print("=> loading checkpoint '{}'".format(pretrain_netG))
+        checkpoint = torch.load(pretrain_netG)
+        start_epoch = checkpoint['epoch']
+        netG.load_state_dict(checkpoint['G_state_dict'])
+        optimizerG.load_state_dict(checkpoint['optimizerG'])
+        print("=> loaded checkpoint '{}' (epoch {})"
+            .format(pretrain_netG, checkpoint['epoch']))
+
+    training_generator = DataGenerator(lexicon_count=lexicon_count, motion_length=motion_length, claim_length=claim_length, num_data=num_train_data, data_dir=train_data_dir, batch_size=batch_size, shuffle=True)
+    test_generator = DataGenerator(lexicon_count=lexicon_count, motion_length=motion_length, claim_length=claim_length, num_data=num_test_data, data_dir=test_data_dir, batch_size=batch_size, shuffle=True)
+
+    for iteration in xrange(start_epoch,epochs+1):
+        print(" >>> Epoch : %d/%d" % (iteration+1,epochs))
+        start_time = time.time()
+
+        print("\nPretraining Generator :")
+        training_steps = num_train_data//batch_size
+        for training_step in range(training_steps):
+            llprint("\rTraining step Generator %d/%d" % (training_step+1, training_steps))
+            netG.zero_grad()
+
+            _motion, _claim = training_generator.generate().next()
+            _motion = np.asarray(_motion)
+            _claim = np.asarray(_claim)
+            real_motion = torch.LongTensor(_motion.tolist())
+            real_claim_G = torch.from_numpy(np.argmax(_claim, 2))
+            real_claim_D = torch.from_numpy(_claim).type('torch.LongTensor')
+
+            if use_cuda:
+                real_motion = real_motion.cuda(gpu)
+                real_claim_G = real_claim_G.cuda(gpu)
+                real_claim_D = real_claim_D.cuda(gpu)
+            real_motion_v = autograd.Variable(real_motion)
+            real_claim_G_v = autograd.Variable(real_claim_G)
+            real_claim_D_v = autograd.Variable(real_claim_D)
+
+            fake = netG(real_motion_v, real_claim_G_v)
+
+            G_loss_1 = nllloss(fake, real_claim_D_v, claim_length)/batch_size
+
+            G_loss_1.backward()
+            G_cost = G_loss_1
+            optimizerG.step()
+
+        print('Iter: {}; G_loss: {:.4}'.format(iteration+1, G_cost.cpu().data.numpy()[0]))
+        with open('pretrain_G_loss_logger.txt', 'a') as out:
+            out.write(str(iteration+1) + ',' + str(G_cost.cpu().data.numpy()[0]) + '\n')
+
+        if iteration % 100 == 0:
+            result_text = open("pretrain_" + datetime.datetime.now().strftime("%Y-%m-%d:%H:%M:%S") + ".txt", "w")
+            testing_steps = num_test_data//batch_size
+            for test_step in range(testing_steps):
+                llprint("\rTesting step %d/%d" % (test_step+1, testing_steps))
+
+                motion_test, claim_test = test_generator.generate().next()
+                _motion_test = np.asarray(motion_test)
+                _claim_test = np.asarray(claim_test)
+                real_motion_test = torch.LongTensor(_motion_test.tolist())
+                real_claim_test = torch.from_numpy(_claim_test)
+                if use_cuda:
+                    real_motion_test = real_motion_test.cuda(gpu)
+                    real_claim_test = real_claim_test.cuda(gpu)
+                real_motion_test_v = autograd.Variable(real_motion_test)
+                real_claim_test_v = autograd.Variable(real_claim_test)
+
+                for mot, cla in zip(decode_motion(motion_test), decode(netG(real_motion_test_v, real_claim_test_v, 0.0))):
+                    result_text.write("Motion: %s\n" % mot)
+                    result_text.write("Generated Claim: %s\n\n" % cla)
+        if iteration % 10 == 0:
+            save_pretrain_checkpoint_G({
+                'epoch': iteration + 1,
+                'G_state_dict': netG.state_dict(),
+                'optimizerG' : optimizerG.state_dict(),
+            })
+
+    return netD, netG
 
 def train(run_name, netG, netD, motion_length, claim_length, embedding_dim, hidden_dim_G, hidden_dim_D, lam, batch_size, epochs, iteration_d, num_words, train_data_dir, test_data_dir):
     jsdloss = JSDLoss()
@@ -96,16 +296,16 @@ def train(run_name, netG, netD, motion_length, claim_length, embedding_dim, hidd
     test_generator = DataGenerator(lexicon_count=lexicon_count, motion_length=motion_length, claim_length=claim_length, num_data=num_test_data, data_dir=test_data_dir, batch_size=batch_size, shuffle=True)
 
     start_epoch = 0
-    if os.path.isfile('last_checkpoint_vanilla_encdec_25Jan.pth.tar'):
-            print("=> loading checkpoint '{}'".format('last_checkpoint_vanilla_encdec_25Jan.pth.tar'))
-            checkpoint = torch.load('last_checkpoint_vanilla_encdec_25Jan.pth.tar')
+    if os.path.isfile(train_GAN):
+            print("=> loading checkpoint '{}'".format(train_GAN))
+            checkpoint = torch.load(train_GAN)
             start_epoch = checkpoint['epoch']
             netD.load_state_dict(checkpoint['D_state_dict'])
             netG.load_state_dict(checkpoint['G_state_dict'])
             optimizerD.load_state_dict(checkpoint['optimizerD'])
             optimizerG.load_state_dict(checkpoint['optimizerG'])
             print("=> loaded checkpoint '{}' (epoch {})"
-                  .format('last_checkpoint_vanilla_encdec_25Jan.pth.tar', checkpoint['epoch']))
+                  .format(train_GAN, checkpoint['epoch']))
 
     for iteration in xrange(start_epoch,epochs+1):
         print(" >>> Epoch : %d/%d" % (iteration+1,epochs))
@@ -283,7 +483,7 @@ if __name__ == '__main__':
     else:
         lexicon_dictionary = pickle.load(open(os.path.join(processed_data_dir, 'lexicon-dict.pkl'), 'rb'))
 
-        lexicon_count = len(lexicon_dictionary)
+        lexicon_count = len(lexicon_dictionary)+1
 
         netG = GeneratorEncDecTeacherForcing(batch_size, lexicon_count, motion_length, claim_length, hidden_dim_G, embedding_dim)
         netD = Discriminator(batch_size, lexicon_count, claim_length, hidden_dim_D)
